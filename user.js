96400d9a4578525ef287270fd9c01ccdb7176036
Laura Kazlausk <laura12331@gmail.com>
2022-10-26 20:59:38 +0300

    style added

--- user.js
+++ user.js
@@ -31,12 +31,21 @@ fetch('https://jsonplaceholder.typicode.com/users/' + userId)
         userCompanyName.textContent =`Company's name: ${user.company.name}`;
 
         let userAddress = document.createElement('p');
-   
-     
-        console.log(user.address)/*reikes masyvo */
-  
+        userAddress.textContent = `Adress: ${user.address.street}, ${user.address.suite},
+         ${user.address.city},${user.address.zipcode}.`
+       
 
-    userInfo.append(userName, userNickname, userEmail, userPhone, userWebsite, userCompanyName )
+        //  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1002063.4758765146!2d80.0472423339837!3d-37.3650270828796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x39e931c3f0a55e62!2zMzfCsDE4JzU3LjIiUyA4McKwMDgnNTguNiJF!5e0!3m2!1slt!2slt!4v1666805581102!5m2!1slt!2slt" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            /*reikes masyvo */
+
+        console.log(user.address)
+        console.log(user.address.street)
+        console.log(user.address.city)
+        console.log(user.address.suite)
+        console.log(user.address.zipcode)
+        console.log(user.address.geo)
+
+    userInfo.append(userName, userNickname, userEmail, userPhone, userWebsite, userCompanyName, userAddress)
     })
     
 
